67acc046fc95f6b2467472b281c9b06f3596645a
akashdomadiya <136041940+akashdomadiya@users.noreply.github.com>
2023-10-11 20:03:03 +0530

    wileumount

--- src/App.js
+++ src/App.js
@@ -1,55 +1,27 @@
 import React, { Component } from "react";
 import "./App.css";
+import Unmount from "./Unmount";
 class App extends Component {
   // Example 1
   constructor() {
     super();
     this.state = {
-      name: "Code4education",
-      count: 0,
+      show: true,
     };
-    // console.log("constructor Called!!");
-  }
-
-  // Example 1
-  componentDidUpdate(prevProps, prevState) {
-    // Example 1
-    console.log(prevState.count, this.state.count);
-
-    if ((this.state.count, prevState.count)) {
-      alert("working");
-      console.log(
-        "componentDidUpdate Called!!",
-        prevState.count,
-        this.state.count,
-        prevProps
-      );
-    }
-
-    // if (this.state.count < 10) {
-    //   this.setState({ count: this.state.count + 1 });
-    // }
   }
 
   render() {
-    // Example 1
-    // console.log("Render Called!!");
+    console.log("clicked", this.state.show);
     return (
       <>
         <div className="App">
-          <h1>Lifecycle componentDidUpdate Method</h1>
-          <h2>Count: {this.state.count}</h2>
+          <h1>Lifecycle Methods</h1>
+
+          {this.state.show ? <Unmount /> : <h2>Removed</h2>}
 
           {/* Example 2 */}
-          {/* <button onClick={() => this.setState({ name: "Bhaskar Gupta" })}>
-            Update State
-          </button> */}
-
-          {/* Example 3 */}
-          <button
-            onClick={() => this.setState({ count: this.state.count + 1 })}
-          >
-            Update State
+          <button onClick={() => this.setState({ show: !this.state.show })}>
+            Removed Component
           </button>
         </div>
       </>

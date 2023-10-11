56d6ab19b9581876b30f9a5c00e11273b39dd404
akashdomadiya <136041940+akashdomadiya@users.noreply.github.com>
2023-10-11 11:01:10 +0530

    usecomp

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useState } from "react";
 import "./App.css";
-import FormValidation from './FormValidation';
-
+import Users from "./Users";
 function App() {
+  // Example 1 Function to be passed as a props
+  const handleButtonClick = () => {
+    console.log("Button clicked!");
+  };
+
+  // Example 2 useState
+  const [count, setCount] = useState(0);
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+
   return (
-      <div className='App'>
-        <h1>Form validation</h1>
-        <FormValidation/>
+    <>
+      <div className="App">
+        <h1>Pass Function as Props</h1>
 
+        <Users
+          handleClick={handleButtonClick}
+          count={count}
+          increment={incrementCount}
+        />
       </div>
-    )
+    </>
+  );
 }
-
 export default App;

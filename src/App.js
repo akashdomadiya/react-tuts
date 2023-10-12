eb008a3dd0d7e3e4b1f0527f5440b743a82ad999
akashdomadiya <136041940+akashdomadiya@users.noreply.github.com>
2023-10-12 14:05:59 +0530

    mapfunction

--- src/App.js
+++ src/App.js
@@ -1,45 +1,63 @@
 import React from "react";
 import "./App.css";
 
-// Example 2
-import "./style.css";
-
-// Example 4
-import Style from "./style.module.css";
-
-// Example 5
-import "./style.css";
-
 function App() {
-  // Example 3
-  const headline = {
-    color: "#fff",
-    backgroundColor: "blue",
-  };
+  // Example 1
+  const names = ["Alice", "Bob", "Charlie", "Bhaskar"];
+
+  // Exmaple 2 Array of Objects
+  const products = [
+    {
+      id: 1,
+      name: "Product 1",
+      price: 10,
+    },
+    {
+      id: 2,
+      name: "Product 2",
+      price: 20,
+    },
+    {
+      id: 3,
+      name: "Product 3",
+      price: 30,
+    },
+    {
+      id: 4,
+      name: "Product 4",
+      price: 40,
+    },
+  ];
+
+  // Example 3 return map function
+  const fruits = ["Apple", "Banana", "Orange", "Mango"];
+
+  // Using map to tranform the array of fruits into an array of react elements
+  const fruitItems = fruits.map((fruit, index) => <li key={index}>{fruit}</li>);
 
   return (
     <>
       <div className="App">
-        <h1>Styles Type In React.</h1>
+        <h1>Array With Map Function.</h1>
 
-        {/* Example 1 Inline CSS */}
-        <h1 style={{ color: "#fff", backgroundColor: "green" }}>
-          Inline Method
-        </h1>
+        {/* Example 1 */}
+        <ul>
+          {names.map((name, index) => (
+            <li key={index}>{name}</li>
+          ))}
+        </ul>
 
-        {/* Example 2 Normal CSS */}
-        <h1 className="primary">Normal Method</h1>
+        {/* Example 2 */}
+        <ul>
+          {products.map((product) => (
+            <li key={product.id}>
+              {product.name} - ${product.price}
+            </li>
+          ))}
+        </ul>
 
-        {/* Example 3 CSS In JS */}
-        <h1 style={headline}>CSS In JS Method</h1>
-
-        {/* Example 4 CSS Module */}
-        <h1 className={Style.title}>CSS Module Method</h1>
-
-        {/* Example 5 Sass & Scss */}
-        <h1 className="primary-sass">
-          Sass & <span>Scss</span> Method
-        </h1>
+        {/* Example 3 */}
+        <ul>{fruitItems}</ul>
       </div>
     </>
   );

cf1d0072b7946e556391a4623c9bf52339ea3a06
akashdomadiya <136041940+akashdomadiya@users.noreply.github.com>
2023-10-13 09:09:07 +0530

    reusecom

--- src/App.js
+++ src/App.js
@@ -1,47 +1,28 @@
 import React from "react";
 import "./App.css";
-
-import Table from 'react-bootstrap/Table';
+import ReusableButton from "./ReusableButton";
+import ReusableList from "./ReusableList";
 
 function App() {
   // Example 1
-  const tableData = [
-    { id: 1, name: "John Doe", age: 25, location: "New York" },
-    { id: 2, name: "Jane Smith", age: 25, location: "London" },
-    { id: 3, name: "Amit Kumar", age: 20, location: "India" },
-    { id: 4, name: "John Wick", age: 28, location: "USA" },
-    // Add more data here if needed
-  ];
+  const handleButtonClick = () => {
+    console.log("Button Clicked!");
+  };
+
+  // Example 2
+  const countries = ["USA", "Canada", "India", "UK", "Aust", "Sri"];
 
   return (
     <>
       <div className="App">
-        <h1>Bootstrap Table In React.</h1>
+        <h1>Reuse Component In React.</h1>
 
-        <div>
-          <Table striped="columns" responsive>
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Name</th>
-                <th>Age</th>
-                <th>Location</th>
-              </tr>
-            </thead>
-            <tbody>
-              {tableData.map((data) =>
-                data.age === 25 ? (
-                  <tr key={data.id}>
-                    <td>{data.id}</td>
-                    <td>{data.name}</td>
-                    <td>{data.age}</td>
-                    <td>{data.location}</td>
-                  </tr>
-                ) : null
-              )}
-            </tbody>
-          </Table>
-        </div>
+        {/* Example 1 */}
+        <ReusableButton text="Click Me" onClick={handleButtonClick} />
+        <ReusableButton text="Submit" onClick={handleButtonClick} />
+
+        {/* Example 2 */}
+        <ReusableList items={countries} />
       </div>
     </>
   );

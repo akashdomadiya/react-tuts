236e7c0eb40c60945c0d442073d76a19279230ee
akashdomadiya <136041940+akashdomadiya@users.noreply.github.com>
2023-10-15 09:11:00 +0530

    use callback

--- src/ChildComponent.js
+++ src/ChildComponent.js
@@ -1,26 +1,13 @@
-import React, { forwardRef, useRef, useImperativeHandle } from "react";
+import React, { memo } from "react";
 
-function ChildComponent(props, ref) {
-  // Example 2
-  const childRef = useRef();
-
-  const handleChildButtonClick = () => {
-    console.log("child button clicked");
-  };
-
-  useImperativeHandle(ref, () => ({
-    handleChildButtonClick,
-  }));
+function ChildComponent({ increment }) {
+  console.log("Child Rendered!!");
 
   return (
     <>
-      {/* Example 1 */}
-      {/* <input type="text" ref={ref} /> */}
-
-      {/* Example 2 */}
-      <button ref={childRef}>Click Me</button>
+      <button onClick={increment}>Increment</button>
     </>
   );
 }
 
-export default forwardRef(ChildComponent);
+export default memo(ChildComponent);

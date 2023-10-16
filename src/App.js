c9b0f7feb629c6f6d443db3f25459bc274deb544
akashdomadiya <136041940+akashdomadiya@users.noreply.github.com>
2023-10-16 18:38:40 +0530

    Fetch API Get Method!

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
 import React from "react";
 import "./App.css";
-import Email from "./Email";
-
+import GetMethod from "./GetMethod";
 function App() {
   return (
     <div className="App">
-      <h1>useID Hook</h1>
+      <h1>Fetch API Get Method!</h1>
 
       {/* Example 1 */}
-      <Email />
-
-      <article>
-        Let’s walk through an example to see when this is useful. HTML
-        accessibility attributes like aria-describedby let you specify that two
-        tags are related to each other. For example, you can specify that an
-        element (like an input) is described by another element (like a
-        paragraph).
-      </article>
-
-      <Email />
+      <GetMethod />
     </div>
   );
 }

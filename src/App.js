8c24c1fda543121d227a9baf195d19da0020eee7
akashdomadiya <136041940+akashdomadiya@users.noreply.github.com>
2023-10-17 18:49:12 +0530

    linkachore

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
-import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
-import GetDataAPI from "./GetDataAPI";
+import Home from "./Component/Home";
+import About from "./Component/About";
+import Navbar from "./Component/Navbar";
+import Service from "./Component/Service";
 function App() {
   return (
     <div className="App">
-      <h1>Axios Library</h1>
+      <h1>Hello! React Router V6</h1>
 
-      {/* Example 1 */}
-      <GetDataAPI />
+      {/* Navigations */}
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/service" element={<Service />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
+
 export default App;
